bf2a003c8508f80c108430d6358fd58aa293b3a1
Grayson <grayson@pop-os.home>
2024-03-12 07:02:50 -0500

    Initial commit of my NeoVim configuration

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("options")
+
+require("grayson.core")
+require("grayson.lazy")
+
+print("Hello nerd")
+ 
+
---
+++ lua/grayson/plugins/colorscheme.lua
@@ -0,0 +1,25 @@
+return {
+	"projekt0n/github-nvim-theme", priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("github_dark_high_contrast")
+	end,
+}
+
+--Themes
+--"Mofiqul/dracula.nvim, "dracula" or "dracula-soft
+--"EdenEast/nigthfox.nvim, nightfox, carbonfox, more
+--"ellisonleao/gruvbox.nvim"
+--"luisiacc/gruvbox-baby"
+--"folke/tokyonight.nvim", has alacritty,storm/night/moon/day
+--"rose-pine/neovim", standard, moon, dawn
+--"marko-cerovac/material", oceanic, deep ocean, palenight, darker
+--"Mofiqul/vscode.nvim" light/dark, alacritty
+--"olimorris/onedark" onedark, onelight, vivid,onedark dark
+--"LunarVim/synthwave84.nvim"
+--"projekt0n/github-nvim-theme" dark, dark_default, dark_high_contrast
+--"rmehri01/onenord.nvim" alacritty
+--"kdheepak/monochrome.nvim" 
+--"rebelot/kanagawa.nvim" dragon, wave, lotus
+--"navarasu/onedark.nvim" style: dark, darker, cool, deep, warm, warmer
+--nyoom-engineering/oxocarbon.nvim or nyoom.nvim"
+--"shaunsingh/nord.nvim"
